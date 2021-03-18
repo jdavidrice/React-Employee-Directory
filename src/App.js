ba6521e4c26ed03288e4bf6fd0ff22a3e304814a
Jeremy Rice <jdavidrice@gmail.com>
2021-03-17 23:03:08 -0400

    Add learn react back to app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<a>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
           Coming Soon
 				</a>
 			</header>
