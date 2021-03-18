0be58920e62d97a1f30c50bba601479d7cfcde3c
Jeremy Rice <jdavidrice@gmail.com>
2021-03-18 00:42:08 -0400

    Add coming soon to app.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-          Learn React
-				</a>
+				<h1>Coming Soon</h1>
 			</header>
+			<br />
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+        Learn React
+			</a>
 		</div>
 	);
 }
